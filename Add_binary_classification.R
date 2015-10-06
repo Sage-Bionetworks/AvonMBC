5b9930533bae69317b5178aa1619c35b8f725280
Thomas Yu <thomasyu888@gmail.com>
2015-10-05 17:09:34 -0700

    Adding binary classification of each annotation category

---
+++ Add_binary_classification.R
@@ -0,0 +1,14 @@
+library(dplyr)
+grantInfo<-read.csv("Metastatic_grant.csv")
+#Only get needed info
+neededInfo <- grantInfo[,c(2:17)]
+#Dplyr dataframe
+needed <- tbl_df(neededInfo)
+#Add column
+needed<-mutate(needed, pw_binary = Pathway)
+#Remove NA', other??
+needed$Pathway[which(is.na(needed$pw_binary) | is.null(needed$pw_binary) | 
+               tolower(needed$pw_binary) == "not specified" | 
+               tolower(needed$pw_binary) == "n/a" | needed$pw_binary == "-")] <- as.factor(0)
+needed$Pathway
+
