a0395bf5cada446f73439d74c94d75e5d7e09611
Thomas Yu <thomasyu888@gmail.com>
2015-10-12 14:12:46 -0700

    Addermalization analysis, and updated matching annotation functions

--- Matching_annotations.R
+++ Matching_annotations.R
@@ -1,6 +1,10 @@
 library(rjson)
 library(stringr)
+library(tm)
+library(RTextTools)
 source("match_annote_functions.R")
+source("textmining_functions.R")
+
 #--------------------------------------------------------
 #    Workflow
 #--------------------------------------------------------
@@ -38,11 +42,11 @@ grantInfo <- read.csv("Metastatic_grant.csv")
 #----------------------
 #Matching keyterms
 #----------------------
-pathway_terms <- matching_keyterms(grantInfo, pathway)
-pwgroup_terms <- matching_keyterms(grantInfo, pathway_group)
-moleculartarget_terms <- matching_keyterms(grantInfo, molecular_target_group)
-mtgroup_terms <- matching_keyterms(grantInfo, molecular_target)
-metastage_terms <- matching_keyterms(grantInfo, metastasis_stage)
+pathway_terms <- matching_keyterms(grantInfo, pathway,F)
+pwgroup_terms <- matching_keyterms(grantInfo, pathway_group,F)
+moleculartarget_terms <- matching_keyterms(grantInfo, molecular_target_group,F)
+mtgroup_terms <- matching_keyterms(grantInfo, molecular_target,F)
+metastage_terms <- matching_keyterms(grantInfo, metastasis_stage,F)
 #meta_terms <- matching_keyterms(grantInfo, list_annotes)
 
 #--------------------------------------
@@ -61,41 +65,89 @@ addition<-sapply(c(1:length(f)), function(x) {
   sink()
 })
 
-pw_compare <- compare("Pathway","match_pathway")
-pwgroup_compare<-compare("Pathway (Group)","match_pwgroup")
-mtgroup_compare<-compare("Molecular Target (Group)","match_mtgroups")
-mt_compare<-compare("Molecular Target","match_mts")
-metastage_compare<-compare("*Metastasis stage","match_metastage")
+#compare human annotated vs machine
+pw_compare <- compare("Pathway","match_pathway","new_jsons")
+pwgroup_compare<-compare("Pathway (Group)","match_pwgroup","new_jsons")
+mtgroup_compare<-compare("Molecular Target (Group)","match_mtgroups","new_jsons")
+mt_compare<-compare("Molecular Target","match_mts","new_jsons")
+metastage_compare<-compare("*Metastasis stage","match_metastage","new_jsons")
 
+#Get accuracy of matching
 getvalues(pw_compare)
 getvalues(pwgroup_compare)
 getvalues(mtgroup_compare)
 getvalues(mt_compare)
 getvalues(metastage_compare)
 
-getvalues <- function(compare) {
-  print(paste("accuracy with NA:",mean(compare>0.5),sep=" "))
-  print(paste("# NA:",sum(compare==0.5),sep=" "))
-  temp<-compare[compare!=0.5]
-  print(paste("accuracy without NA:",mean(temp>0),sep=" "))
-}
-
 #---------------------------------------------------------
 # Normalize data then do workflow
 #---------------------------------------------------------
-library(tm)
-library(RTextTools)
-source("textmining_functions.R")
-
 mainData <- as.vector(paste(grantInfo$AwardTitle,grantInfo$TechAbstract))
 mainData <- toAmericanEnglish(mainData)
 norm_mainData <- normalise_text(mainData)
 
+#Vectorize annotation categories
 Pathway <- as.vector(grantInfo$Pathway)
+mt <- as.vector(grantInfo$Molecular.Target)
+pwgroup <- as.vector(grantInfo$Pathway..Group.)
+mtgroup <- as.vector(grantInfo$Molecular.Target..Group.)
+meta <- as.vector(grantInfo$X.Metastasis.stage)
+
+#Normalize human annotated categories
 norm_Pathway <- normalise_text(Pathway)
-View(norm_mainData)
-View(norm_Pathway)
-class(norm_Pathway)
-list_norm_pw <- unlist(unique(norm_Pathway),use.names = F)
+norm_pwgroup <- normalise_text(pwgroup)
+norm_mtgroup <- normalise_text(mtgroup)
+norm_mt <- normalise_text(mt)
+norm_meta <- normalise_text(meta)
+
+#Process normalised annotation lists
+list_norm_pw <- trim(unlist(unique(norm_Pathway),use.names = F))
+list_norm_pw<- list_norm_pw[list_norm_pw != ""]
+
+list_norm_pwgroup <- trim(unlist(unique(norm_pwgroup),use.names = F))
+list_norm_pwgroup <- list_norm_pwgroup[list_norm_pwgroup!=""]
+
+list_norm_mt <- trim(unlist(unique(norm_mt),use.names = F))
+list_norm_mt <- list_norm_mt[list_norm_mt!=""]
+
+list_norm_mtgroup <- trim(unlist(unique(norm_mtgroup),use.names = F))
+list_norm_mtgroup <- list_norm_mtgroup[list_norm_mtgroup!=""]
+
+list_norm_meta <- trim(unlist(unique(norm_meta),use.names = F))
+list_norm_meta <- list_norm_meta[list_norm_meta!=""]
+
+#Match keyterms
+norm_pw_match <- matching_keyterms(norm_mainData,list_norm_pw,T)
+norm_pwgroup_match <- matching_keyterms(norm_mainData,list_norm_pwgroup,T)
+norm_mt_match <- matching_keyterms(norm_mainData,list_norm_mt,T)
+norm_mtgroup_match <- matching_keyterms(norm_mainData, list_norm_mtgroup,T)
+norm_meta_match <- matching_keyterms(norm_mainData, list_norm_meta,T)
 
 
+
+### Normalized add to JSON
+addition<-sapply(c(1:2237), function(x) {
+  temp <- fromJSON(file=sprintf("dataexample/json/%d.json",x))
+  temp$match_pathway = paste0(norm_pw_match[[x]],collapse = ",")
+  temp$match_pwgroup = paste0(norm_pwgroup_match[[x]],collapse = ",")
+  temp$match_mts = paste0(norm_mt_match[[x]],collapse = ",")
+  temp$match_mtgroups = paste0(norm_mtgroup_match[[x]],collapse = ",")
+  temp$match_metastage = paste0(norm_meta_match[[x]],collapse = ",")
+  sink(sprintf("dataexample/Norm_json/%d.json",x))
+  cat(toJSON(temp))
+  sink()
+})
+
+
+#COMPARE
+pwn_compare <- compare("Pathway","match_pathway","Norm_json",T)
+pwgroupn_compare<-compare("Pathway (Group)","match_pwgroup","Norm_json",T)
+mtgroupn_compare<-compare("Molecular Target (Group)","match_mtgroups","Norm_json",T)
+mtn_compare<-compare("Molecular Target","match_mts","Norm_json",T)
+metastagen_compare<-compare("*Metastasis stage","match_metastage","Norm_json",T)
+
+getvalues(pwn_compare)
+getvalues(pwgroupn_compare)
+getvalues(mtn_compare)
+getvalues(mtgroupn_compare)
+getvalues(metastagen_compare)
--- match_annote_functions.R
+++ match_annote_functions.R
@@ -1,4 +1,5 @@
-
+library(rjson)
+library(stringr)
 #--------------------------------------------------------
 #get Specific annotation
 #--------------------------------------------------------
@@ -55,12 +56,16 @@ matching_keyterms <- function(grantInfo,list_annotes,normalized) {
   } else {
     grants <- grantInfo
   }
-  keyterms<- sapply(grants, function(x) {
+  keyterms<- apply(as.matrix(grants),1, function(x) {
     words <- sapply(list_annotes, function(y) {
       #coll searches for just the word
       return(str_count(x,sprintf("\\b%s\\b",y)))
     })
-    return(names(sort(words[words>0],decreasing = T)[1:5]))
+    if (normalized) {
+      return(names(words[words>0]))
+    } else {
+      return(names(sort(words[words>0],decreasing = T)[1:5]))
+    }
   })
   return(keyterms)
 }
@@ -68,22 +73,45 @@ matching_keyterms <- function(grantInfo,list_annotes,normalized) {
 #--------------------------------------
 #Compare keyterms with human annotated
 #--------------------------------------
-compare <- function(human_annote, machine_annote) {
-  accuracy<-lapply(c(1:length(new_files)), function(x) {
-    temp <- fromJSON(file=sprintf("dataexample/new_jsons/%d.json",x))
+compare <- function(human_annote, machine_annote,folder,noNA) {
+  accuracy<-lapply(c(1:2237), function(x) {
+    temp <- fromJSON(file=sprintf("dataexample/%s/%d.json",folder,x))
     machine<-unlist(temp[sprintf("%s",machine_annote)],use.names=F)
     human <- unlist(temp[sprintf("%s",human_annote)],use.names=F)
     toms<- unlist(strsplit(machine,","))
     num<-sapply(toms, function(y) {
       grep(sprintf("\\b%s\\b",y),human,ignore.case =T)
     })
-    if (is.null(human)) {
-      return(0.5)
-    } else if (human == "-" || tolower(human) =="not specified" || tolower(human) == "n/a") {
-      return(0.5)
-    } else {
-      return(sum(unlist(num)))
-    }
+    lower <- tolower(human)
+    if (noNA) {
+      if (is.null(human)) {
+        return(0.5)
+      } else if (human == "-" || lower =="not specified" || lower == "n/a"  ||
+                 lower == "other/not specified" || lower == "not relevant" ||
+                 lower == "no abstract" || lower == "n/a (chemo delivery system)" || 
+                 lower == "n/a (imaging of mets)"|| lower == "n/a (imaging)" || 
+                 lower =="n/a (imaging of mets) & imaging herceptin delivery"||
+                 lower == "n/a (therapy delivery system)" || lower == "n/a surgery"|| 
+                 lower == "none"|| lower =="not applicable" || 
+                 lower == "0_no specific target") {
+        return(0.5)
+      } #else {
+        #return(sum(unlist(num)))
+      #}
+    } 
+    return(sum(unlist(num)))
   })
   return(accuracy)
+}
+
+
+#------------------------------
+# Getting Accuracy of matching
+#------------------------------
+
+getvalues <- function(compare) {
+  print(paste("accuracy with NA:",mean(compare>0.5),sep=" "))
+  print(paste("# NA:",sum(compare==0.5),sep=" "))
+  temp<-compare[compare!=0.5]
+  print(paste("accuracy without NA:",mean(temp>0),sep=" "))
 }
