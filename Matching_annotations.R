0bc646fe47ade39ea6012aff584766ffc1336777
Thomas Yu <thomasyu888@gmail.com>
2015-10-11 23:22:03 -0700

    Make matching_annotations R file only a work flow file

--- Matching_annotations.R
+++ Matching_annotations.R
@@ -1,89 +1,6 @@
 library(rjson)
 library(stringr)
-
-#--------------------------------------------------------
-#get Specific annotation
-#--------------------------------------------------------
-trim <- function (x) gsub("^\\s+|\\s+$", "", x)
-
-getAnnote <- function(filenames,type) {
-  annotes<-lapply(filenames, function(x) {
-    temp <- fromJSON(file=x)
-    return(temp[sprintf("%s",type)])
-  })
-  return(unlist(annotes,use.names = F))
-}
-
-#------------------------------------
-#Extract all unique pathways
-#------------------------------------
-annote_extract <- function(annote) {
-  #Sub parenthesis with ","
-  parenthesis <- gsub("\\(|\\)",",",annote)
-  parenthesis <- parenthesis[!duplicated(tolower(parenthesis))]
-  parenthesis <- unlist(strsplit(parenthesis,","))
-  
-  #Remove white spaces
-  newlist<-trim(parenthesis)
-  newlist <- newlist[!duplicated(tolower(newlist))]
-  
-  #Replace "or","and", and "/" with ","
-  process <- gsub(" or ", ",", newlist,fixed=T)
-  process <- gsub(" and ", ",",process, fixed=T)
-  process <- gsub("/",",",process, fixed=T)
-  
-  #Split strings on ","
-  process <- unlist(strsplit(process,","))
-  process<- process[!duplicated(tolower(process))]
-  
-  #Manually curate
-  final<-process[process != "-"]
-  final <- final[final != "a"]
-  final <- final[final != "n"]
-  final <- final[final != "K"]
-  final <- final[final != "3"]
-  final <- final[final != "-1"]
-  final <- final[final != "-2"]
-  final <- final[final != ""]
-  return(final)
-}
-
-#---------------------------------------
-#Match top 5 key terms
-#--------------------------------------
-matching_keyterms <- function(grantInfo,list_annotes) {
-  keyterms<- sapply(paste(grantInfo$TechAbstract,grantInfo$AwardTitle,sep=","), function(x) {
-    words <- sapply(list_annotes, function(y) {
-      #coll searches for just the word
-      return(str_count(x,sprintf("\\b%s\\b",y)))
-    })
-    return(names(sort(words[words>0],decreasing = T)[1:5]))
-  })
-  return(keyterms)
-}
-
-#--------------------------------------
-#Compare keyterms with human annotated
-#--------------------------------------
-compare <- function(human_annote, machine_annote) {
-  accuracy<-lapply(c(1:length(new_files)), function(x) {
-    temp <- fromJSON(file=sprintf("dataexample/new_jsons/%d.json",x))
-    machine<-unlist(temp[sprintf("%s",machine_annote)],use.names=F)
-    human <- unlist(temp[sprintf("%s",human_annote)],use.names=F)
-    toms<- unlist(strsplit(machine,","))
-    num<-sapply(toms, function(y) {
-      grep(sprintf("\\b%s\\b",y),human,ignore.case =T)
-    })
-    if (is.null(human)) {
-      return(0.5)
-    } else if (human == "-" || tolower(human) =="not specified" || tolower(human) == "n/a") {
-      return(0.5)
-    } else {
-      return(sum(unlist(num)))
-    }
-  })
-  return(accuracy)
-}
+source("match_annote_functions.R")
 #--------------------------------------------------------
 #    Workflow
 #--------------------------------------------------------
@@ -171,8 +88,14 @@ library(RTextTools)
 source("textmining_functions.R")
 
 mainData <- as.vector(paste(grantInfo$AwardTitle,grantInfo$TechAbstract))
-mainData <- toAmericanEnglish(textVec)
+mainData <- toAmericanEnglish(mainData)
 norm_mainData <- normalise_text(mainData)
 
+Pathway <- as.vector(grantInfo$Pathway)
+norm_Pathway <- normalise_text(Pathway)
+View(norm_mainData)
+View(norm_Pathway)
+class(norm_Pathway)
+list_norm_pw <- unlist(unique(norm_Pathway),use.names = F)
 
 
