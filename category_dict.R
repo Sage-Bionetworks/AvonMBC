2cec28ec455849294e2976190caaf3554b3352ec
Thomas Yu <thomasyu888@gmail.com>
2015-10-13 21:36:16 -0700

    Do mapping of pathways to pathway groups

--- category_dict.R
+++ category_dict.R
@@ -1,16 +1,37 @@
 library(xlsx)
+source("textmining_functions.R")
 mt_excel<-read.xlsx("MetastaticBC_Grants_CodingFile_Complete_Sagebase.xlsx",4)
 pw_excel<-read.xlsx("MetastaticBC_Grants_CodingFile_Complete_Sagebase.xlsx",5)
 grantInfo <- read.csv("Metastatic_grant.csv",check.names = F)
+#-----------------------------------
+#extract keywords from excel
+#-----------------------------------
 pathways<- colnames(grantInfo)[grep("^KW",colnames(grantInfo))]
 pathways <- substring(pathways,4)
 pathways <- pathways[-1]
-pathways
-View(mt_excel$Molecular.Target)
+write.csv(pathways,"dictionary/KW.csv",row.names = F)
+
+#-------------------------------------------------------------
+# Molecular target - Molecular target group mapping
+#-------------------------------------------------------------
 mt_excel$Molecular.Target..Group.
 
-write.csv(pathways,"dictionary/KW.csv",row.names = F)
-read.csv("dictionary/pathway_dict.csv")
 
-pw_checked<- read.csv("dictionary/pw_dict_check.csv")
-write.csv(factor(pw_checked$x[!duplicated(tolower(pw_checked$x))]),"dictionary/pw_dict_check.csv",row.names = F)
+
+
+#---------------------------------------------
+# Mapping pathway to pathway group
+#---------------------------------------------
+#Hand curated pathway dictionary
+t<-read.csv("dictionary/pathway_dict.csv")
+
+pw_group_map <- apply(as.matrix(t$x),1, function(x) {
+  groups <- pw_excel$Pathway..Group.[grep(sprintf("\\b%s\\b",x),pw_excel$Pathway)]
+  return(paste(unique(groups),collapse=","))
+})
+
+pw_pwgroup_mapped<- cbind(as.character(t$x),pw_group_map)
+#write.csv(pw_pwgroup_mapped,"dictionary/pw_pwgroup_mapping.csv",row.names=F)
+
+#list_norm_pw<- normalise_text(list_norm_pw)
+
