0bab80c337894410e09345244dcf14ed5c868236
Thomas Yu <thomasyu888@gmail.com>
2015-11-06 14:46:59 -0800

    Add in best parameter searcher using gridSearchCV

--- python/Grant_binary_prediction.py
+++ python/Grant_binary_prediction.py
@@ -17,21 +17,26 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import NearestCentroid
 from sklearn.utils.extmath import density
 from sklearn import metrics
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
 #NLTK -> tokenizer, stemmer
 import nltk
 from nltk.collocations import *
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
-nltk.download("stopwords")
+#nltk.download("stopwords")
 import pandas
 import string
 import re
+from time import time
+import logging
 #Beautiful soup -> Gets rid of HTML and replaces non ascii characters
-pip install Beautifulsoup4
+#pip install Beautifulsoup4
 from bs4 import BeautifulSoup
 import unicodedata 
 import numpy
+import pprint
 
 
 #-------------------------------------------------------------------------------
@@ -48,38 +53,84 @@ def is_number(s):
 #Normalization of text (Remove HTML, replace non-ascii characters, remove punctuation,
 #remove lone numbers, stem words, remove stop words recombine into text)
 #--------------------------------------------------------------------------------------
-grantInfo = pandas.read_csv('metastatic_grant_binary.csv')
+grants = pandas.read_csv('metastatic_grant_binary.csv')
 total = grants['AwardTitle'] + ", " + grants['TechAbstract']
 stopwords.words('english')
 #Remove HTML, replace non-ascii characters, tokenize (remove punctuation), stem words, then reconcatenate
 text_noHTML = [BeautifulSoup(i).get_text() for i in total]
 total_ascii = [unicodedata.normalize('NFKD', text).encode('ascii','ignore') for text in text_noHTML]
-total_noperiod = [re.sub(r'\D\.\D',' ', text) for text in total_ascii]
+total_noNumbers = [re.sub(r'\d+', '', text) for text in total_ascii]
+total_noperiod = [re.sub(r'\D\.\D',' ', text) for text in total_noNumbers]
 total_noslash = [re.sub(r'\b\/\b',' ', text) for text in total_noperiod]
-total_combinedash = [re.sub(r'\b-\b',"",text) for text in total_noslash]
+total_combinedash = [re.sub(r'\b-\b','',text) for text in total_noslash]
 total_nopunct = [nltk.word_tokenize(text.translate(None, string.punctuation)) for text in total_combinedash]
 
 
 
 #Use english stemmer
 stemmer = SnowballStemmer("english")
-total_complete[:]
+total_complete=[]
 for i in range(0,len(total_nopunct)):
-	temp  = [is_number(text)  for text in total_nopunct[i] ]
-	temp = [stemmer.stem(word) for word in temp]
+	#temp  = [is_number(text)  for text in total_nopunct[i] ]
+	temp = [stemmer.stem(word) for word in total_nopunct[i]]
 	temp = [word for word in temp if word not in stopwords.words('english')]
-	total_complete[i] = ' '.join(temp)
+	total_complete.append(' '.join(temp))
 
+
+train_data = total_complete[0:1500]
+train_result = grantInfo['pw_binary'][0:1500]
+test_data = total_complete[1501:2236]
+test_result = grantInfo['pw_binary'][1501:2236]
+
+#----------------------------------------------------------------
+# Best Parameters
+#----------------------------------------------------------------
+pipeline = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('clf', SGDClassifier()),
+])
+
+# uncommenting more parameters will give better exploring power but will
+# increase processing time in a combinatorial way
+parameters = {
+    'vect__max_df': (0.5, 0.75, 1.0),
+    'vect__max_features': (None, 5000, 10000, 50000),
+    'vect__ngram_range': ((1, 1), (1, 2)),  # unigrams or bigrams
+    'tfidf__use_idf': (True, False),
+    'tfidf__norm': ('l1', 'l2'),
+    'clf__alpha': (0.00001, 0.000001),
+    'clf__penalty': ('l2', 'elasticnet'),
+    'clf__n_iter': (10, 50, 80),
+}
+# multiprocessing requires the fork to happen in a __main__ protected
+# block
+
+# find the best parameters for both the feature extraction and the
+# classifier
+grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=1)
+
+print("Performing grid search...")
+print("pipeline:", [name for name, _ in pipeline.steps])
+print("parameters:")
+print(parameters)
+t0 = time()
+grid_search.fit(train_data, train_result)
+print("done in %0.3fs" % (time() - t0))
+print()
+
+print("Best score: %0.3f" % grid_search.best_score_)
+print("Best parameters set:")
+best_parameters = grid_search.best_estimator_.get_params()
+for param_name in sorted(parameters.keys()):
+	print("\t%s: %r" % (param_name, best_parameters[param_name]))
 
 
 
 #----------------------------------------------------------------
 # SCIKIT TOOLS -> Get features by using TfidF Vectorizer
 #----------------------------------------------------------------
-train_data = total_complete[0:1500]
-train_result = grantInfo['pw_binary'][0:1500]
-test_data = total_complete[1501:2236]
-test_result = grantInfo['pw_binary'][1501:2236]
+
 
 
 vectorizer = CountVectorizer(max_df=0.0005,stop_words='english')
